65c21331ce093f81d80a957083853ba22949a703
Caitlin Amelia <ameliamolon@gmail.com>
2024-11-18 15:49:49 +0000

    Main and multiplication files have been made.

---
+++ multiplication.java
@@ -0,0 +1,37 @@
+public class multiplication {
+
+    private char multiplier;
+    private int firstValue;
+    private int secondValue;
+
+    private float valueOne;
+    private float valueTwo;
+
+
+public multiplication(int firstValue, int secondValue, float valueOne, float valueTwo){
+
+for(int count=0; count<=secondValue; count++){
+    firstValue= firstValue + firstValue;
+    int integerResult = firstValue;
+}
+
+for(int count=0; count<=valueTwo; count++){
+    valueOne = valueOne + valueOne;
+        float floatResult = valueOne;
+    }
+
+    this.firstValue = firstValue;
+    this.secondValue = secondValue;
+    this.valueOne = valueOne;
+    this.valueTwo = valueTwo;
+}
+
+}
+
+//pseudocode:
+// step 1. get symbol * to symbolise multiplication
+// basically when * is scanned this invokes the multiplication method
+// Multiplication(mxn) is m adding to itself n number of times
+//how to achieve this - possibly a for loop that has a count=no. of times adding(n) and adds m each cycle.
+//
+//
