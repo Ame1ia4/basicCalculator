59789a35a958a875b78898f679aa8c7705fccb33
Caitlin Amelia <ameliamolon@gmail.com>
2024-12-03 18:44:24 +0000

    symbol recognition and addition to index

--- symbolRecognition.java
+++ symbolRecognition.java
@@ -9,11 +9,13 @@ public class symbolRecognition {
         Addition addition = new Addition();
         Subtraction subtraction = new Subtraction();
 
-        for (int i = 0; i< splitInput.size(); i++) {
+            for(int i = 0; i< splitInput.size(); i++){
             if (splitInput.get(i).equals("+")) {
-                splitInput.set(i,Double.toString(Double.parseDouble(splitInput.get(i-1))+Double.parseDouble(splitInput.get(i+1))));
+                splitInput.set(i - 1, Double.toString(Double.parseDouble(splitInput.get(i - 1)) + Double.parseDouble(splitInput.get(i + 1))));
+                splitInput.remove(i + 1);
+                splitInput.remove(i);
             } else if (splitInput.get(i).equals("-")) {
-                splitInput.set(i,Double.toString(Double.parseDouble(splitInput.get(i-1))-Double.parseDouble(splitInput.get(i+1))));
+                splitInput.set(i, Double.toString(Double.parseDouble(splitInput.get(i - 1)) - Double.parseDouble(splitInput.get(i + 1))));
             } else if (i == '*') {
                 multiplication.getFirstInput();
             } else if (i == '/') {
@@ -21,12 +23,12 @@ public class symbolRecognition {
             }
         }
 
-        for(String currentChar : splitInput ){
+
+        for (String currentChar : splitInput) {
             System.out.println(currentChar);
-
         }
-
     }
 }
+
 //Apply in main?
 //Adapt maths classes so that (i-1) and (i+1) rather than first and second input
