a728ae03315b8a25cc9ac33a754ba267cca2030f
patrick <patricksyms8@gmail.com>
2024-12-18 20:00:15 +0000

    finished comments for subtraction and addition

--- AddSubtract.java
+++ AddSubtract.java
@@ -22,16 +22,18 @@ public class AddSubtract extends Calculation {
                     expression.set(i - 1, Double.toString(Double.parseDouble(expression.get(i + 1)) + Double.parseDouble(expression.get(i - 1))));
                     expression.remove(i + 1);//removes the now obsolete number
                     expression.remove(i);// removes the addition sign
-                } else if (expression.get(i).equals("-")) {
-                    if(i == expression.size() - 1 && Parsing.isNumeric(expression.get(i-1)) ){
-                        expression.set(expression.size()-1,Double.toString(Double.parseDouble(expression.get(i-1)) * (-1)));
-                        expression.remove(i-1);
-                    }
-                    //---5  5---
-                    else if (!Parsing.isNumeric(expression.get(i+1))){
+                }
+                //if the element is a "-"
+                else if (expression.get(i).equals("-")) {
+                    //if the last element of the reversed array is a "-" and the next element is a number
+                    // or if the element after the reversed array the "-" is not a number then it will
+                    if((i == expression.size() - 1 && Parsing.isNumeric(expression.get(i-1))) ||(!Parsing.isNumeric(expression.get(i+1)))){
+                        //sets the last element to the number * -1
                         expression.set(i,Double.toString(Double.parseDouble(expression.get(i-1)) * (-1)));
-                        expression.remove(i-1);
+                        expression.remove(i-1);//removes the number as its now where the "-" was
                     }
+                    //otherwise set the element before the "-"  in the reversed array to the result of the operation and get rid of the "-"
+                    //and the element after
                     else {
                         expression.set(i - 1, Double.toString(Double.parseDouble(expression.get(i + 1)) - Double.parseDouble(expression.get(i - 1))));
                         expression.remove(i + 1);
